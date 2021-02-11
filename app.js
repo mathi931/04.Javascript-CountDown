16ac591219b59ef6757a5e8c92ce4290286a2462
unknown <mesi.mathi@gmail.com>
2021-02-11 14:06:48 +0100

    created some data

---
+++ app.js
@@ -0,0 +1,23 @@
+
+const DatesToSelect = [
+    {
+        id: 0,
+        name: 'Tomorrowland',
+        date: '16 July 2021'
+    },
+    {
+        id: 1,
+        name: 'Christmas',
+        date: '25 Dec 2021'
+    },
+    {
+        id: 2,
+        name: 'New year`s eve',
+        date: '01 Jan 2022'
+    },
+    {
+        id: 3,
+        name: 'Birthday',
+        date: '26 June 2021'
+    },
+]
