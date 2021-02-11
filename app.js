24adef364a4d1aa8a3053c50c1e6cab9c66ed8ed
unknown <mesi.mathi@gmail.com>
2021-02-11 16:15:10 +0100

    some changes and tests

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+//some data
 const DatesToSelect = [
     {
         id: 0,
@@ -17,28 +18,46 @@ const DatesToSelect = [
     {
         id: 3,
         name: 'Birthday',
-        date: '26 June 2021'
+        date: '26 March 2021'
     },
 ]
+
+//dropdown element
 const dropDown = document.getElementById('selector');
+
+//index array
 let iarr = [];
 
+//dropdownd change event
 dropDown.addEventListener('change', function (e){
     //save the current index
     iarr.push(e.target.value);
     //clear the last index to stop the loop
     clearInterval(iarr[iarr.length-1]);
     //start the loop
-    setInterval(function(){
-    Countdown(DatesToSelect[e.target.value].date)
-} ,1000);
+    setInterval(function(){LoadValues(DatesToSelect[e.target.value].date)} ,1000);
+    //load the elements
+    LoadElements()
 });
-function Countdown(date){
+
+//Load values and elements
+function LoadValues(date){
     document.getElementById('days').innerHTML = GetDifferenceInDays(GetTotalDifferenceSec(date));
     document.getElementById('hours').innerHTML = GetDifferenceInHours(GetTotalDifferenceSec(date));
     document.getElementById('minutes').innerHTML = GetDifferenceInMinutes(GetTotalDifferenceSec(date));
     document.getElementById('seconds').innerHTML = GetDifferenceInSeconds(GetTotalDifferenceSec(date));
     }
+function LoadElements(){
+        const spans = document.querySelectorAll('span');
+        document.getElementById("countdownCont").className = "countdown-container-border";
+        spans[0].innerHTML = 'days';
+        spans[1].innerHTML = 'hours';
+        spans[2].innerHTML = 'minutes';
+        spans[3].innerHTML = 'seconds';
+        
+    }
+
+//Get the values
 function GetTotalDifferenceSec(date){
     const currentDate = new Date().getTime();
     const countDownDate =new Date(date);
